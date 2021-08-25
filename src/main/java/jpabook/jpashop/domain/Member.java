6375fc9844ce42a9c877d5d1bd39999ae5bc8a9e
Sangin-C <sangin3435@naver.com>
2021-08-25 22:18:34 +0900

    챕터2. 도메인 분석 설계 - 엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")  //mappedBy는 '나는 주인이 아니다'. Order 엔티티에 있는 Member member에 맵핑된거다라는 의미이다. ( 읽기 전용이다 )
+    private List<Order> orders = new ArrayList<>();
+}
